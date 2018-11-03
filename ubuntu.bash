30d264c4dfb069142e90513921a1f5a9b766e3c6
SaurabhAgarwala <saur.agarwala@gmail.com>
2018-11-03 18:50:57 +0530

    Ubuntu Updation Script Added

---
+++ ubuntu.bash
@@ -0,0 +1,14 @@
+https://linuxconfig.org/how-to-upgrade-to-ubuntu-18-04-lts-bionic-beaver
+https://www.digitalocean.com/community/tutorials/how-to-upgrade-to-ubuntu-18-04
+
+
+sudo apt-get update
+{sudo password}
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo do-release-upgrade
+{Enter}
+{y}
+{Enter}
+(Removing the packages part){y}
+(System restart required){y}
