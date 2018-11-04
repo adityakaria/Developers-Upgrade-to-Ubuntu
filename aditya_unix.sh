9cca531016f6e9d5b59e0db5b783e975601f794d
adityakaria <adityakaria01@gmail.com>
2018-11-04 13:49:05 +0530

    Added anaconda installation

--- aditya_unix.sh
+++ aditya_unix.sh
@@ -57,4 +57,14 @@ git:
     git config --global user.name "$name"
     git config --global user.email "$mail"
 
+anaconda:
+    cd /tmp
+    curl -O https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+    bash Anaconda3-5.2.0-Linux-x86_64.sh
+    {press enter}
+    {yes}
+    {yes}
+    {yes/no for installation of VS Code}
+    source ~/.bashrc
+    //conda info
 
